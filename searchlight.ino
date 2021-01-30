93b04c4b63bf7414aee9d18eae1cb730bcfa0088
Bruno Albuquerque <bga@gmail.com>
2021-01-30 07:48:07 -0800

    Various changes:
    - Do a fade effect when the game starts ro simulate the "fuse blown".
    - Bat color is now magenta.
    - Move all references from marker to bat.

--- searchlight.ino
+++ searchlight.ino
@@ -6,6 +6,13 @@
 #define GAME_STATE_END_LOSE 3
 
 #define PLAY_TIME_IN_SECONDS 60
+#define FADE_TO_BLACK_TIME_IN_MS 500
+
+#define LIGHT_COLOR WHITE
+#define BAT_COLOR MAGENTA
+#define FLOOR_COLOR YELLOW
+#define WIN_COLOR BLUE
+#define LOSE_COLOR RED
 
 typedef byte GameState;
 static GameState game_state_;
@@ -15,8 +22,8 @@ union FaceState {
     byte game_state : 2;
     bool send_light : 1;
     bool is_lit : 1;
-    bool send_marker : 1;
-    bool has_marker : 1;
+    bool send_bat : 1;
+    bool has_bat : 1;
     byte reserved : 2;
   };
 
@@ -27,15 +34,17 @@ static FaceState out_face_state_[FACE_COUNT];
 
 struct BlinkState {
   bool is_player;
-  bool has_marker;
+  bool has_bat;
   bool time_tracker;
-  byte move_marker_face;
+  bool game_started;
+  byte move_bat_face;
 };
 static BlinkState blink_state_;
 
 static const byte opposite_face_[] = {3, 4, 5, 0, 1, 2};
 
 static Timer game_play_timer_;
+static Timer fade_to_black_timer_;
 
 static void set_game_state(byte game_state) { game_state_ = game_state; }
 
@@ -64,96 +73,110 @@ static bool is_lit() {
 void setup() {
   randomize();
 
-  blink_state_.move_marker_face = FACE_COUNT;
+  blink_state_.move_bat_face = FACE_COUNT;
 }
 
-static void move_marker() {
-  // Keep track of the number of possible moves and the faces the marker can
+static void move_bat() {
+  // Keep track of the number of possible moves and the faces the bat can
   // move through.
   byte possible_move_count = 0;
   int8_t possible_move_face[FACE_COUNT];
 
   FOREACH_FACE(face) {
     if (!isValueReceivedOnFaceExpired(face) && !in_face_state_[face].is_lit) {
-      // Face is not expired and is not lit. Marker can move through there.
+      // Face is not expired and is not lit. Bat can move through there.
       possible_move_face[possible_move_count] = face;
       possible_move_count++;
     }
   }
 
   if (possible_move_count == 0) {
-    // The marker can not be moved anywhere. Game over.
+    // The bat can not be moved anywhere. Game over.
     set_game_state(GAME_STATE_END_WIN);
 
     return;
   }
 
   // Pick a random face from the ones we can move to.
-  byte move_marker_face = possible_move_face[random(possible_move_count - 1)];
+  byte move_bat_face = possible_move_face[random(possible_move_count - 1)];
 
-  // Send the marker through there.
-  out_face_state_[move_marker_face].send_marker = true;
+  // Send the bat through there.
+  out_face_state_[move_bat_face].send_bat = true;
 
   // And remember the face.
-  blink_state_.move_marker_face = move_marker_face;
+  blink_state_.move_bat_face = move_bat_face;
 }
 
 static void render_blink_state() {
   switch (get_game_state()) {
     case GAME_STATE_IDLE:
-      // When idle, just render the Blink as yellow.
+      // When idle, just render the Blink as the floor.
       //
       // TODO(bga): It might be nice to animate the "flashlight" face as if it
       // was searching for something.
-      setColor(YELLOW);
+      setColor(FLOOR_COLOR);
       break;
     case GAME_STATE_PLAY:
       // Play state. Things are a bit more complex now.
       if (is_lit()) {
         // This Blink is lit.
         if (blink_state_.is_player) {
-          // It is a player Blink. Reset all face to off.
+          // It is a player Blink. Reset all faces to off.
           setColor(OFF);
 
           // And set face 0 to white.
           setColorOnFace(WHITE, 0);
-        } else if (blink_state_.has_marker) {
-          // It has the marker and was caught by a flashlight. Render it as red.
-          setColor(RED);
+        } else if (blink_state_.has_bat) {
+          // It has the bat and it was caught by a flashlight. Render it as the
+          // bat.
+          setColor(BAT_COLOR);
         } else {
           // Just set the color to white to show we are lit.
-          setColor(WHITE);
+          setColor(LIGHT_COLOR);
         }
-      } else if (blink_state_.has_marker) {
-        // This Blink is not lit and has the marker. Render as off.
-        setColor(OFF);
+      } else if (blink_state_.has_bat) {
+        // This Blink is not lit and has the bat. Render it according to the
+        // fade to black timer (i.e. it will start at the given color and fade
+        // to off).
+        setColor(dim(BAT_COLOR,
+                     (MAX_BRIGHTNESS * fade_to_black_timer_.getRemaining()) /
+                         FADE_TO_BLACK_TIME_IN_MS));
       } else {
-        // Not lit and no marker. Render as off.
-        setColor(OFF);
+        // Not lit and no bat. Render it according to the fade to black timer
+        // (i.e. it will start at the given color and fade to off).
+        setColor(dim(FLOOR_COLOR,
+                     (MAX_BRIGHTNESS * fade_to_black_timer_.getRemaining()) /
+                         FADE_TO_BLACK_TIME_IN_MS));
       }
       break;
     case GAME_STATE_END_WIN:
       // We won!
-      if (blink_state_.has_marker) {
-        // If we have the marker, render as red so all players can see where it
+      if (blink_state_.has_bat) {
+        // If we have the bat, render it so all players can see where it
         // was.
-        setColor(RED);
+        setColor(BAT_COLOR);
       } else {
-        // No marker. Render as blue, which is ourt victory color.
-        setColor(BLUE);
+        // No bat. Render as the win color.
+        setColor(WIN_COLOR);
       }
       break;
     case GAME_STATE_END_LOSE:
-      // We lost. Render as red.
-      //
-      // TODO(bga): Maybe show where the actual marker was?
-      setColor(RED);
+      // We lost.
+      if (blink_state_.has_bat) {
+        // If we have the bat, render it so all players can see where it
+        // was.
+        setColor(BAT_COLOR);
+      } else {
+        // No bat. Render as the lose color.
+        setColor(LOSE_COLOR);
+      }
       break;
   }
 
+  // Update reporting state for all faces.
   FOREACH_FACE(face) {
     out_face_state_[face].game_state = get_game_state();
-    out_face_state_[face].has_marker = blink_state_.has_marker;
+    out_face_state_[face].has_bat = blink_state_.has_bat;
     out_face_state_[face].is_lit = is_lit();
 
     setValueSentOnFace(out_face_state_[face].as_byte, face);
@@ -162,10 +185,11 @@ static void render_blink_state() {
 
 static void idle_loop() {
   // Reset our state.
-  blink_state_.has_marker = false;
+  blink_state_.has_bat = false;
   blink_state_.is_player = false;
   blink_state_.time_tracker = false;
-  blink_state_.move_marker_face = FACE_COUNT;
+  blink_state_.game_started = true;
+  blink_state_.move_bat_face = FACE_COUNT;
 
   if (buttonSingleClicked() && !hasWoken()) {
     // The button was single-clicked and it was not a wake-up click.
@@ -173,11 +197,11 @@ static void idle_loop() {
       // We are alone, so we are a player Blink now.
       blink_state_.is_player = true;
     } else {
-      // We are not alone, so we are a piece of the board that has the marker.
-      blink_state_.has_marker = true;
+      // We are not alone, so we are a piece of the board that has the bat.
+      blink_state_.has_bat = true;
 
       // And we will also keep track of the timer.
-      game_play_timer_.set(PLAY_TIME_IN_SECONDS * 1000);
+      game_play_timer_.set(1000L * PLAY_TIME_IN_SECONDS);
       blink_state_.time_tracker = true;
     }
 
@@ -195,20 +219,26 @@ static void play_player_loop() {
 }
 
 static void play_board_loop() {
-  // if (!move_timer_.isExpired()) return;
+  if (blink_state_.game_started) {
+    // Game just started. enable fade to black animation.
+    fade_to_black_timer_.set(FADE_TO_BLACK_TIME_IN_MS);
 
-  if (blink_state_.has_marker && blink_state_.move_marker_face == FACE_COUNT) {
-    // We hold the marker and we are not transferring it yet. Check if whatever
-    // Blink that send it to us still thinks it holds it.
+    // And remember we already started.
+    blink_state_.game_started = false;
+  }
+
+  if (blink_state_.has_bat && blink_state_.move_bat_face == FACE_COUNT) {
+    // We hold the bat and we are not transferring it yet. Check if
+    // whatever Blink that send it to us still thinks it holds it.
     FOREACH_FACE(face) {
-      if (in_face_state_[face].has_marker) {
+      if (in_face_state_[face].has_bat) {
         // It does. Do not try to move it to another Blink yet.
         return;
       }
     }
 
-    // Move marker to another Blink.
-    move_marker();
+    // Move bat to another Blink.
+    move_bat();
   }
 }
 
@@ -249,19 +279,18 @@ static void process_neighbors() {
       }
     }
 
-    if (blink_state_.move_marker_face == face &&
-        in_face_state_[face].has_marker) {
-      // We got confirmation that the marker transferred to the other Blink.
+    if (blink_state_.move_bat_face == face && in_face_state_[face].has_bat) {
+      // We got confirmation that the bat transferred to the other Blink.
       // Complete the move.
-      blink_state_.has_marker = false;
-      blink_state_.move_marker_face = FACE_COUNT;
+      blink_state_.has_bat = false;
+      blink_state_.move_bat_face = FACE_COUNT;
 
-      out_face_state_[face].send_marker = false;
+      out_face_state_[face].send_bat = false;
     }
 
-    if (in_face_state.send_marker && !blink_state_.has_marker) {
-      // This neighbor Blink is sending us the marker. Accept it.
-      blink_state_.has_marker = true;
+    if (in_face_state.send_bat && !blink_state_.has_bat) {
+      // This neighbor Blink is sending us the bat. Accept it.
+      blink_state_.has_bat = true;
     }
 
     // If this face is sending light to us, we propagate it to the opposite
